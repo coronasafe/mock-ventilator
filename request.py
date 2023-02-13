e4862458a8806187e061c3bfd2891d5d7dcf6550
Mathew Alex <matthewzalex@gmail.com>
2023-02-13 11:39:23 +0530

    env option for  middleware ip

--- request.py
+++ request.py
@@ -1,12 +1,14 @@
 import requests
 import json
 import time
+import os
 
 with open('mock_data/data.json', 'r') as f:
     data = '[' + f.read()[:-1] + ']'
     data = json.loads(data)
 
-API_ENDPOINT = "http://127.0.0.1:8090/update_observations"
+API_ENDPOINT = "http://{0}:8090/update_observations".format(os.getenv('middleware_ip', 'teleicu_middleware'))
+# API_ENDPOINT = "http://127.0.0.1:8090/update_observations"
 
 while True:
     for req_data in data:
